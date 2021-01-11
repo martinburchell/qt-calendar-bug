40e3c59ae6631282d0e297da2245137db48f9ad2
Martin Burchell <mb2353@cam.ac.uk>
2021-01-11 17:30:44 +0000

    Add QDateTimeEdit

--- main.cpp
+++ main.cpp
@@ -1,16 +1,23 @@
 #include <QApplication>
 #include <QCalendarWidget>
+#include <QDateTimeEdit>
 
 int main(int argc, char* argv[])
 {
     QApplication app(argc, argv);
+    QDateTimeEdit editor;
+
+    editor.setCalendarPopup(true);
+
     QCalendarWidget calendar;
     calendar.setSelectionMode(QCalendarWidget::SingleSelection);
     calendar.setNavigationBarVisible(true);
     calendar.setFirstDayOfWeek(Qt::Monday);
     calendar.setGridVisible(true);
 
-    calendar.show();
+    editor.setCalendarWidget(&calendar);
+
+    editor.show();
 
     return app.exec();
 }
